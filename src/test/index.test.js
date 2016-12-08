5fe4cc5c0977599e219b6657f499871b452e450b
Andre Broers <broersa@Andres-MacBook-Air.local>
2016-12-08 18:27:21 +0100

    unit tests added

---
+++ src/test/index.test.js
@@ -0,0 +1,28 @@
+/* eslint-env node, mocha */
+var chai = require('chai');
+// var assert = chai.assert;
+var expect = chai.expect;
+var sinon = require('sinon');
+var index = require('../index.js');
+
+describe('index', function () {
+  var sandbox;
+
+  beforeEach(function () {
+    // Create a sandbox for the test
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function () {
+    // Restore all the things made through the sandbox
+    sandbox.restore();
+  });
+
+  it('should run', function () {
+    // sandbox.stub(config, 'get', function (param) {
+    //   return '';
+    // });
+    var x = index.bulkUpdate();
+    expect(x).to.be.false;
+  });
+});
